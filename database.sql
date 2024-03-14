009ccc45e49faf514c66fa6da798f342f9fba738
Cali Knowlden <calbearnc@gmail.com>
2024-03-13 19:20:30 -0500

    added additional resources and information to database

--- database.sql
+++ database.sql
@@ -2,9 +2,13 @@ CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL,
-    "email" VARCHAR (80) NOT NULL,
-    "role_id" INT NOT NULL
+    "role_id" INT DEFAULT 2
 );
+
+INSERT INTO "user" ("username", "password")
+    VALUES ('user', 'password') RETURNING id;
+    
+DROP TABLE "user";
 
 CREATE TABLE "roles" (
     "id" SERIAL PRIMARY KEY,
@@ -14,8 +18,6 @@ CREATE TABLE "roles" (
 INSERT INTO "roles" ("type")
 VALUES ('guest'),('reg_user'),('admin');
 
-INSERT INTO "user" ("username", "password", "email", "role_id")
-    VALUES ('user', 'password', 'user@gmail.com', 2) RETURNING id;
 
 CREATE TABLE "rsvp" (
 	"id" SERIAL PRIMARY KEY,
@@ -26,11 +28,18 @@ CREATE TABLE "rsvp" (
 	"user_id" INT
 );
 
+DROP TABLE "rsvp";
+
+INSERT INTO "rsvp" ("first_name", "last_name", "email", "phone")
+VALUES ('firstName','lastName', 'test@gmail.com', 1234567890);
+
 CREATE TABLE "rsvp_events" (
 	"id" SERIAL PRIMARY KEY,
 	"event_id" INT,
 	"rsvp_id" INT
 );
+
+DROP TABLE "rsvp_events";
 
 CREATE TABLE "events" (
 	"id" SERIAL PRIMARY KEY,
@@ -46,9 +55,29 @@ CREATE TABLE "events" (
 	"details" VARCHAR (300)
 	);
 
-INSERT INTO "rsvp" ("first_name", "last_name", "email", "phone")
-VALUES ('firstName','lastName', 'test@gmail.com', 1234567890);
+CREATE TABLE "resources" (
+	"title" VARCHAR (80) NOT NULL,
+	"link" VARCHAR (255) NOT NULL,
+	"description" VARCHAR NOT NULL
+	);
 
-DROP TABLE "rsvp";
+INSERT INTO "resources" ("title", "link", "description")
+VALUES ('Prevent Suicide Wisconsin (PSW)', 'https://www.preventsuicidewi.org/home','Prevent Suicide Wisconsin (PSW) is a statewide public-private partnership whose mission is to reduce the number of people who attempt and/or die by suicide in Wisconsin.'),
+	('Wester Regional Agricultural Stress Assistance Program','https://farmstress.us/', 'WRASAP recognizes that high levels of stress have been present in our agricultural communities. Causes include unstable finances, carrying the pressure of multigenerational farm lineage, injury, chronic/acute illness, adverse weather and climate change, and recent COVID-19 stressors. We want to make sure that you, your family, and people you work with have access to the resources needed, when they’re needed.'),
+	('Apple Podcasts: Cutivating Resilience','https://podcasts.apple.com/us/podcast/cultivating-resilience/id1623202122','If you’re a farmer, you know the joy of working the land: the cadence of the seasons, the understanding of the natural world, the tangible results of your hard work. But you probably also know how stressful it can be. From family and finance pressures, to isolation and an exhausting job that has no days off.');
 
-DROP TABLE "rsvp_events";
+DROP TABLE "resources";
+
+CREATE TABLE "information" (
+	"fact" BOOLEAN default true,
+	"description" VARCHAR NOT NULL
+	);
+
+INSERT INTO "information" ("description")
+VALUES ('Suicide is the 11th leading cause of death in the US'),
+	('In 2021, men died by suicide 3.90x more than women'),
+	('On average, there are 132 suicides per day'),
+	('Caucasian males accounted for 69.68% of suicide deaths in 2021'),
+	('In 2021, firearms accounted for 54.64% of all suicide deaths'),
+	('94% of adults surveyed in the U.S. believe that suicide can be prevented');
+
