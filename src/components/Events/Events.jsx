7ced24d75b4dd40c8d5251a32936fccef67eee41
Cali Knowlden <calbearnc@gmail.com>
2024-03-19 20:58:27 -0500

    events are now rendered on DOM

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-// This is one of our simplest components
-// It doesn't have local state,
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is'
+function Events() {
+  const events = useSelector((store) => store.events);
+  const dispatch = useDispatch();
 
-function Home() {
+  useEffect(() => {
+    dispatch({ type: 'FETCH_EVENTS' });
+  }, []);
   return (
     <div className="container">
       <div>
         <p>Here's a list of events in your area</p>
+        {/* {JSON.stringify(events)} */}
+        <section className="events">
+          {events.map((event) => {
+            return (
+              <div key={event.id}>
+                <h3>{event.name}</h3>
+                <p>{event.date}</p>
+                <p>{event.location}</p>
+                <p>{event.street} </p>
+                <p>
+                  {event.city},{event.state}
+                  {event.zip}
+                </p>
+                {/* <img
+                data-testid="toDetails"
+                id={movie.id}
+                src={movie.poster}
+                alt={movie.title}
+                onClick={handleClick}
+              /> */}
+              </div>
+            );
+          })}
+        </section>
       </div>
     </div>
   );
 }
 
-export default Home;
+export default Events;
