5b90b6cd7425ea480ad118e7de8d069e5b8e3590
Cali Knowlden <calbearnc@gmail.com>
2024-03-26 18:34:23 -0500

    added go back x button in rsvp page

--- src/components/Rsvp/Rsvp.jsx
+++ src/components/Rsvp/Rsvp.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 function Rsvp() {
+  const history = useHistory();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch({ type: 'SET_TITLE', payload: 'RSVP' });
   }, []);
 
+  const goBack = () => {
+    history.push('/events');
+  };
+
   return (
     <div className="container">
+      <Button className="goBackButton" onClick={goBack} variant="outlined">
+        X
+      </Button>
       <p>Hello please RSVP here</p>
     </div>
   );
