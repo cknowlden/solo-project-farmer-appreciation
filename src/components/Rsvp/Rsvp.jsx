c20ad8c70645c59bde9c3713c0dfa7faeebf0e5d
Cali Knowlden <calbearnc@gmail.com>
2024-03-26 19:05:05 -0500

    added an rsvp button to rsvp page

--- src/components/Rsvp/Rsvp.jsx
+++ src/components/Rsvp/Rsvp.jsx
@@ -1,23 +1,32 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Button from '@mui/material/Button';
 
 function Rsvp() {
-  const { id } = useParams();
   const history = useHistory();
   const dispatch = useDispatch();
+
+  const eventDetails = useSelector((store) => store.details);
+  const details = eventDetails[0] || 'No details available';
 
   useEffect(() => {
     dispatch({ type: 'SET_TITLE', payload: 'RSVP' });
   }, []);
 
-  // useEffect(() => {
-  //   dispatch({ type: 'FETCH_DETAILS', payload: id });
-  // }, []);
-
   const goBack = () => {
     history.push('/events');
+  };
+
+  const handleClick = (event) => {
+    const id = details.id;
+    event.preventDefault();
+    dispatch({
+      type: 'SET_RSVP',
+      payload: {
+        id: id,
+      },
+    });
   };
 
   return (
@@ -25,7 +34,12 @@ function Rsvp() {
       <Button className="goBackButton" onClick={goBack} variant="outlined">
         X
       </Button>
-      <p>Hello please RSVP here</p>
+      <p>RSVP for:</p>
+      <p>{details.name}</p>
+      {/* <p>{details.id}</p> */}
+      <Button className="btn" onClick={handleClick} variant="outlined">
+        RSVP
+      </Button>
     </div>
   );
 }
