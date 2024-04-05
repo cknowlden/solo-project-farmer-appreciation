9da6d411cf19514873db570daf0a264dd230fa17
Cali Knowlden <calbearnc@gmail.com>
2024-04-04 19:58:52 -0500

    centered create event title and adjusted background rectangles

--- src/components/CreateEvent/CreateEvent.jsx
+++ src/components/CreateEvent/CreateEvent.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import TextField from '@mui/material/TextField';
-import { Button, Snackbar, Alert, Box, Typography } from '@mui/material';
+import { Button, Snackbar, Alert, Box, Grid, Typography } from '@mui/material';
 import './CreateEvent.css';
 import '../App/App.css';
 
@@ -89,166 +89,154 @@ function CreateEvent() {
           X
         </Button>
       </Box>
-      <div className="big-rect">
-        <Box
-          sx={{
-            backgroundImage: "url('images/sunset-lake.jpg')",
-            height: '700px',
-            width: '100%',
-            marginLeft: '100px',
-            marginRight: '300px',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: '100% 100%',
-            borderRadius: '10px',
-          }}
-        >
-          <Box
-            alignItems="center"
-            justifyContent={'center'}
-            sx={{
-              bgcolor: 'honeydew',
-              height: '78vh',
-              width: '600px',
-              marginLeft: '100px',
-              marginRight: '300px',
-              backgroundRepeat: 'no-repeat',
-              display: 'flex',
-              flexDirection: 'column',
-              borderRadius: '10px',
-            }}
-          >
+
+      <Box
+        sx={{
+          display: 'left',
+          border: 1,
+          borderRadius: '16px',
+          borderColor: 'honeydew',
+          marginLeft: '18%',
+          marginRight: '18%',
+          minHeight: '60vh',
+          backgroundColor: 'honeydew',
+        }}
+      >
+        <div className="insert-bg-create">
+          <center>
             <Typography variant="h3" fontWeight={600}>
               Add your event here:
             </Typography>
+          </center>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <input
+              className="name"
+              required
+              onChange={handleInputChange}
+              value={formData.name}
+              name="name"
+              type="text"
+              placeholder="Event title (required)"
+            />{' '}
             <br />
-            <form onSubmit={handleSubmit}>
+            <textarea
+              className="details"
+              required
+              onChange={handleInputChange}
+              value={formData.details}
+              name="details"
+              type="text"
+              placeholder="Event description (required)"
+            />
+            <br />
+            <input
+              className="location"
+              onChange={handleInputChange}
+              value={formData.location}
+              name="location"
+              type="text"
+              placeholder="Event location title (optional)"
+            />{' '}
+            <br />
+            <input
+              className="street"
+              required
+              onChange={handleInputChange}
+              value={formData.street}
+              name="street"
+              type="text"
+              placeholder="Location street address (required)"
+            />{' '}
+            <br />
+            <div className="address">
               <input
-                className="name"
                 required
                 onChange={handleInputChange}
-                value={formData.name}
-                name="name"
+                value={formData.city}
+                name="city"
                 type="text"
-                placeholder="Event title (required)"
-              />{' '}
-              <br />
-              <textarea
-                className="details"
-                required
-                onChange={handleInputChange}
-                value={formData.details}
-                name="details"
-                type="text"
-                placeholder="Event description (required)"
+                placeholder="City (required)"
               />
-              <br />
               <input
-                className="location"
-                onChange={handleInputChange}
-                value={formData.location}
-                name="location"
-                type="text"
-                placeholder="Event location title (optional)"
-              />{' '}
-              <br />
-              <input
-                className="street"
                 required
                 onChange={handleInputChange}
-                value={formData.street}
-                name="street"
+                value={formData.state}
+                name="state"
                 type="text"
-                placeholder="Location street address (required)"
-              />{' '}
-              <br />
-              <div className="address">
-                <input
-                  required
-                  onChange={handleInputChange}
-                  value={formData.city}
-                  name="city"
-                  type="text"
-                  placeholder="City (required)"
-                />
-                <input
-                  required
-                  onChange={handleInputChange}
-                  value={formData.state}
-                  name="state"
-                  type="text"
-                  placeholder="State (required)"
-                />
-                <input
-                  required
-                  onChange={handleInputChange}
-                  value={formData.zip}
-                  name="zip"
-                  type="text"
-                  placeholder="Zip (required)"
-                />{' '}
-              </div>
-              Image upload:
+                placeholder="State (required)"
+              />
               <input
+                required
                 onChange={handleInputChange}
-                value={formData.image}
-                name="image"
-                type="file"
-                label="Image upload (optional)"
+                value={formData.zip}
+                name="zip"
+                type="text"
+                placeholder="Zip (required)"
               />{' '}
-              <br />
-              <div className="date-cost">
-                <TextField
+            </div>
+            Image upload:
+            <input
+              onChange={handleInputChange}
+              value={formData.image}
+              name="image"
+              type="file"
+              label="Image upload (optional)"
+            />{' '}
+            <br />
+            <div className="date-cost">
+              <TextField
+                required
+                onChange={handleInputChange}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                value={formData.date}
+                name="date"
+                label="Event Date & Time"
+                variant="outlined"
+                type="datetime-local"
+              />
+              {''}
+              <div className="cost">
+                $
+                <input
+                  className="cost"
                   required
                   onChange={handleInputChange}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  value={formData.date}
-                  name="date"
-                  label="Event Date & Time"
-                  variant="outlined"
-                  type="datetime-local"
+                  value={formData.cost}
+                  name="cost"
+                  type="number"
+                  placeholder="Cost (required)"
                 />
-                {''}
-                <div className="cost">
-                  $
-                  <input
-                    className="cost"
-                    required
-                    onChange={handleInputChange}
-                    value={formData.cost}
-                    name="cost"
-                    type="number"
-                    placeholder="Cost (required)"
-                  />
-                </div>
               </div>
-              <center>
-                <br />
-                <Button type="submit" variant="contained" size="large">
-                  Create Event
-                </Button>
-                <Snackbar
-                  open={open}
-                  autoHideDuration={6000}
+            </div>
+            <center>
+              <br />
+              <Button type="submit" variant="contained" size="large">
+                Create Event
+              </Button>
+              <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleClose}
+              >
+                <Alert
                   onClose={handleClose}
+                  severity="success"
+                  variant="filled"
+                  sx={{ width: '100%' }}
                 >
-                  <Alert
-                    onClose={handleClose}
-                    severity="success"
-                    variant="filled"
-                    sx={{ width: '100%' }}
-                  >
-                    Congrats! Your event has been created. You may return to the
-                    Events Page to view it.
-                  </Alert>
-                </Snackbar>
-              </center>
-            </form>
-          </Box>
-        </Box>
-        {/* </div> */}
-      </div>
+                  Congrats! Your event has been created. You may return to the
+                  Events Page to view it.
+                </Alert>
+              </Snackbar>
+            </center>
+          </form>
+        </div>
+      </Box>
+
+      {/* </div> */}
     </div>
   );
 }
