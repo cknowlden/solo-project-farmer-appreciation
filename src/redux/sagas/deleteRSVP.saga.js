6d548d5f5d4e10aa4892f490672a93b9036667df
Cali Knowlden <calbearnc@gmail.com>
2024-08-15 11:29:05 -0500

    defined saga for deleting rsvps

--- src/redux/sagas/deleteRSVP.saga.js
+++ src/redux/sagas/deleteRSVP.saga.js
@@ -2,18 +2,19 @@ import axios from 'axios';
 import { put, takeEvery } from 'redux-saga/effects';
 
 // worker saga: will be fired on "DELETE_RSVP" actions
-function* deleteEvent(action) {
+function* deleteRSVP(action) {
   try {
+    // what is detailsResponse
     const detailsResponse = yield axios.delete(
-      `/api/details/${action.payload.id}`
+      `/api/rsvp/${action.payload.id}`
     );
-    yield put({ type: 'SET_DELETE_EVENT', payload: detailsResponse.data });
+    yield put({ type: 'SET_DELETE_RSVP', payload: detailsResponse.data });
   } catch (error) {
     console.log('Event could not be deleted', error);
   }
 }
-function* deleteSaga() {
-  yield takeEvery('DELETE_EVENT', deleteEvent);
+function* deleteRSVPSaga() {
+  yield takeEvery('DELETE_RSVP', deleteRSVP);
 }
 
-export default deleteSaga;
+export default deleteRSVPSaga;
